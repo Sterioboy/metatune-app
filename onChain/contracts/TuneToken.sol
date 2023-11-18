76011401203ae851548a2506526467b6c7fda7a3
Cosmodude <vladislav.lenskii@gmail.com>
2023-11-19 05:16:22 +0900

    token contract

---
+++ onChain/contracts/TuneToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TuneToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("TuneToken", "TUNE") Ownable(){}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount){
+        _burn(msg.sender, amount)
+    }
+}
