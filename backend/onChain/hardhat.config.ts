bb4f3e7317e29f2803a760756e764b84afdd14df
Cosmodude <vladislav.lenskii@gmail.com>
2023-11-19 09:45:31 +0900

    filecoin part

--- backend/onChain/hardhat.config.ts
+++ backend/onChain/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 import { load } from 'ts-dotenv';
 
 const env = load({
@@ -20,6 +21,22 @@ const config: HardhatUserConfig = {
       url: "https://linea-goerli.infura.io/v3/" + env.INFURA_API_KEY,
       accounts: [env.WALLET_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: env.ETHERSCAN_API_KEY,
+      linea_testnet: env.ETHERSCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "linea_testnet",
+        chainId: 59140,
+        urls: {
+          apiURL: "https://api-testnet.lineascan.build/api",
+          browserURL: "https://goerli.lineascan.build"
+        }
+      }
+    ]
   }
 }
 
---
+++ backend/onChain/scripts/postToFilecoin.ts
@@ -0,0 +1,83 @@
+// Import the NFTStorage class and File constructor from the 'nft.storage' package
+import { NFTStorage, File } from 'nft.storage'
+
+// The 'mime' npm package helps us set the correct file type on our File objects
+import mime from 'mime'
+
+// The 'fs' builtin module on Node.js provides access to the file system
+import fs from 'fs'
+
+// The 'path' module provides helpers for manipulating filesystem paths
+import path from 'path'
+import { load } from 'ts-dotenv';
+
+const env = load({
+  INFURA_API_KEY: String,
+  INFURA_SECRET_KEY: String,
+  WALLET_PRIVATE_KEY: String,
+  ETHERSCAN_API_KEY: String,
+  NFT_STORAGE_API_KEY: String
+});
+
+// Paste your NFT.Storage API key into the quotes:
+const NFT_STORAGE_KEY = env.NFT_STORAGE_API_KEY
+
+/**
+  * Reads an image file from `imagePath` and stores an NFT with the given name and description.
+  * @param {string} imagePath the path to an image file
+  * @param {string} name a name for the NFT
+  * @param {string} description a text description for the NFT
+  */
+async function storeArrtistData(imagePath: string, name: string, description: string) {
+    // load the file from disk
+    const image = await fileFromPath(imagePath)
+
+    // create a new NFTStorage client using our API key
+    const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
+
+    // call client.store, passing in the image & metadata
+    return nftstorage.store({
+        image,
+        name,
+        description,
+    })
+}
+
+/**
+  * A helper to read a file from a location on disk and return a File object.
+  * Note that this reads the entire file into memory and should not be used for
+  * very large files. 
+  * @param {string} filePath the path to a file to store
+  * @returns {File} a File object containing the file content
+  */
+async function fileFromPath(filePath: string) {
+    const content = await fs.promises.readFile(filePath)
+    const type = mime.getType(filePath)!!
+    return new File([content], path.basename(filePath), { type })
+}
+
+
+/**
+ * The main entry point for the script that checks the command line arguments and
+ * calls storeNFT.
+ * 
+ * To simplify the example, we don't do any fancy command line parsing. Just three
+ * positional arguments for imagePath, name, and description
+ */
+async function main() {
+    const imagePath = "/Users/vlad/DEV/Hackathons/metatune-app/backend/onChain/artist_image.jpeg";
+    const name = "Lily";
+    const description = "obsorbing techno ambience reminding of your trip to the sea"
+
+    const result = await storeArrtistData(imagePath, name, description)
+    console.log(result)
+}
+
+// Don't forget to actually call the main function!
+// We can't `await` things at the top level, so this adds
+// a .catch() to grab any errors and print them to the console.
+main()
+  .catch(err => {
+      console.error(err)
+      process.exit(1)
+  })
